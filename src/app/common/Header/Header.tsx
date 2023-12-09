b82bd9cfb99dfc78d042ff93b0499c951ac150de
TumiMaleka <tumimaleka2002@gmail.com>
2023-12-09 13:56:42 +0200

    Updated Library

--- src/app/common/Header/Header.tsx
+++ src/app/common/Header/Header.tsx
@@ -47,10 +47,10 @@ function Header() {
                   <a href="/Header/Contact" className="block py-2 pr-4 pl-3 hover:text-blue-400 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent text-semibold lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 hover:text-blue-400 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent text-semibold lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Library</a>
+                  <a href="/Header/Library" className="block py-2 pr-4 pl-3 hover:text-blue-400 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent text-semibold lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Library</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 hover:text-blue-400 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent text-semibold lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">News</a>
+                  <a href="/Header/News" className="block py-2 pr-4 pl-3 hover:text-blue-400 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent text-semibold lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">News</a>
                 </li>
               </ul>
             </div>
