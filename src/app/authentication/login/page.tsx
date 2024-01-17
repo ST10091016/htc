eb0f7920ca99c88139d9f33d652629c640471054
TumiMaleka <tumimaleka2002@gmail.com>
2024-01-17 11:12:53 +0200

    login UI updated

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -37,18 +37,22 @@ export default function Page() {
     });
   };
 
+  if (session){
+    router.push('/')
+  }
+
 
   if (!session && status !== "loading") {
     return (
-      <section className="bg-center bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/1137332/pexels-photo-1137332.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-stone-400 bg-blend-multiply">
+      <section className="bg-center bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/946186/pexels-photo-946186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-gray-400 bg-blend-multiply">
         <div className="flex items-center justify-center h-screen ">
-          <div className="w-full mx-4 max-w-sm p-4 align-middle bg-white bg-opacity-10 rounded-lg shadow-xl hover:shadow-2xl sm:p-6 md:p-8  transition-all ease-in-out">
+          <div className="w-full mx-4 max-w-sm p-4 align-middle bg-white bg-opacity-30 rounded-lg shadow-xl hover:shadow-2xl sm:p-6 md:p-8  transition-all ease-in-out">
             <form className="space-y-6">
-              <h5 className="text-xl font-medium text-center text-white ">
-                Sign In
+              <h5 className="text-3xl text-center text-white font-bold">
+                Login
               </h5>
               <div>
-                <label className="block mb-2 text-sm font-medium text-white">
+                <label className="block mb-2 text-sm font-semibold text-white">
                   Account Type
                 </label>
                 <select
@@ -58,30 +62,30 @@ export default function Page() {
                     setData({ ...data, role: e.target.value });
                   }}
                   id="role"
-                  className="bg-gray-50 bg-opacity-20 text-white text-sm rounded-lg block w-full p-2"
+                  className="bg-gray-50 bg-opacity-50 text-white text-sm rounded-lg block w-full p-2"
                   required
                 >
-                  <option className="bg-gray-500 text-white" value=""></option>
+                  <option className="bg-gray-800 text-white" value=""></option>
                   <option
-                    className="bg-gray-500 text-white"
+                    className="bg-gray-800 text-white"
                     value="centerMember"
                   >
                     Center Member
                   </option>
-                  <option className="bg-gray-500 text-white" value="convert">
+                  <option className="bg-gray-800 text-white" value="convert">
                     Convert
                   </option>
-                  <option className="bg-gray-500 text-white" value="secretary">
+                  <option className="bg-gray-800 text-white" value="secretary">
                     Secretary
                   </option>
-                  <option className="bg-gray-500 text-white" value="admin">
+                  <option className="bg-gray-800 text-white" value="admin">
                     Admin
                   </option>
                 </select>
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-white">
-                  Email
+                <label className="block mb-2 text-sm font-semibold text-white">
+                  Email Address
                 </label>
                 <input
                   type="email"
@@ -91,13 +95,13 @@ export default function Page() {
                     setData({ ...data, email: e.target.value });
                   }}
                   id="email"
-                  className="bg-gray-50 bg-opacity-20 text-white text-sm rounded-lg block w-full p-2"
+                  className="bg-gray-50 placeholder-white bg-opacity-50 text-white text-sm rounded-lg block w-full p-2"
                   placeholder="example@example.com"
                   required
                 />
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-white">
+                <label className="block mb-2 text-sm font-semibold text-white">
                   Password
                 </label>
                 <input
@@ -109,15 +113,16 @@ export default function Page() {
                   }}
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 bg-opacity-20 text-white text-sm rounded-lg block w-full p-2"
+                  className="placeholder-white bg-gray-50 bg-opacity-50 text-white text-sm rounded-lg block w-full p-2"
                   required
+                  
                 />
               </div>
               <button
                 onClick={loginUser}
                 className={`${
                   isLoading ? "cursor-not-allowed" : ""
-                } w-full bg-primary text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center`}
+                } w-full text-white bg-gradient-to-r from-green-700 via-green-800 to-green-950 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-3 text-center`}
               >
                 {isLoading ? (
                   <div role="status">
@@ -144,15 +149,8 @@ export default function Page() {
                 )}
               </button>
               <div className="text-sm font-medium text-gray-200">
-                Not registered?{" "}
-                <a
-                  href="/authentication/registration"
-                  className="text-blue-700 hover:underline"
-                >
-                  Create account
-                </a>{" "}
-                Or{" "}
-                <a href="/" className="text-blue-700 hover:underline">
+               
+                <a href="/" className="text-white hover:underline">
                   Back Home
                 </a>
               </div>
