9f79a4554c7d0e25508e3c9f5e81dbec0cfd64ac
TumiMaleka <tumimaleka2002@gmail.com>
2024-02-01 18:23:43 +0200

    fix

--- src/app/(secretary)/secretary/(users)/converts/page.tsx
+++ src/app/(secretary)/secretary/(users)/converts/page.tsx
@@ -5,12 +5,19 @@ import Link from "next/link";
 export default async function Page() {
   return (
     <>
-      <div className="flex py-5 justify-end">
+      <div className="flex py-5 justify-end gap-2">
         <Link
           href="/secretary/converts/add-convert"
           className="block text-white shadow-lg hover:shadow-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none   font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
         >
           Add Convert
+        </Link>
+
+        <Link
+          href="/secretary/converts/table"
+          className="block text-white shadow-lg hover:shadow-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none   font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
+        >
+          Table
         </Link>
       </div>
       <ConvertCard />
